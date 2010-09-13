4e9698bafd02bfaaae233909727630127f744470
Rishav Rastogi <rishav.rastogi@gmail.com>
2010-09-13 10:06:36 +0530

    added socket io and websocket implmenenation

--- server.js
+++ server.js
@@ -1,37 +1,91 @@
-var sys    = require('sys'),
+var http = require('http'), 
+		url = require('url'),
+		fs = require('fs'),
+		io = require('./vendor/Socket.IO-node/'),
+		sys = require('sys'),
 	TwitterNode = require('twitter-node').TwitterNode;
 
-// Command line args
-var USERNAME = process.ARGV[2];
-var PASSWORD = process.ARGV[3];
-var KEYWORD  = process.ARGV[4] || "iphone";
+	var USERNAME = process.ARGV[2];
+	var PASSWORD = process.ARGV[3];
+	var KEYWORD  = process.ARGV[4] || "iphone";	  
 
 if (!USERNAME || !PASSWORD)
-  return sys.puts("Usage: node server.js <twitter_username> <twitter_password> <keyword>");
+ return sys.puts("Usage: node server.js <twitter_username> <twitter_password> <keyword>");
 
 
 var twit = new TwitterNode({
-  user: USERNAME, 
-  password:  PASSWORD,
+ user: USERNAME, 
+ password:  PASSWORD,
 });
-
+var broadcast_clients = [];
 
 twit.track(KEYWORD)
-
+var buffer = [];
 twit
-  .addListener('tweet', function(tweet) {
-    sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
-  })
+ .addListener('tweet', function(tweet) {
+   for(var i=0; i < broadcast_clients.length; i++) {
+		broadcast_clients[i].send({message:[1,"@" + tweet.user.screen_name + ": " + tweet.text]})
+	}
+ })
 
-  .addListener('limit', function(limit) {
-    sys.puts("LIMIT: " + sys.inspect(limit));
-  })
+ .addListener('end', function(resp) {
+   	for(var i=0; i < broadcast_clients.length; i++) {
+			broadcast_clients[i].send({message:[1,"wave goodbye... " + resp.statusCod]})
+		}
+ })
+ .stream();
+		
 
-  .addListener('delete', function(del) {
-    sys.puts("DELETE: " + sys.inspect(del));
-  })
+server = http.createServer(function(req, res){
+	// your normal server code
+	var path = url.parse(req.url).pathname;
+	switch (path){
+		case '/':
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write('<h1>Welcome. Try the <a href="/twitter.html">chat</a> example.</h1>');
+			res.end();
+			break;
+			
+		case '/json.js':
+		case '/twitter.html':
+			fs.readFile(__dirname + path, function(err, data){
+				if (err) return send404(res);
+				res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+				res.write(data, 'utf8');
+				res.end();
+			});
+			break;
+			
+		default: send404(res);
+	}
+}),
 
-  .addListener('end', function(resp) {
-    sys.puts("wave goodbye... " + resp.statusCode);
-  })
-  .stream();
+send404 = function(res){
+	res.writeHead(404);
+	res.write('404');
+	res.end();
+};
+
+server.listen(8081)
+// socket.io, I choose you
+// simplest chat application evar
+var io = io.listen(server);
+		
+		
+io.on('connection', function(client){
+	broadcast_clients.push(client) ;
+	client.send({ buffer: buffer });
+	
+	
+	client.on('message', function(message){
+		var msg = { message: [client.sessionId, message] };
+		buffer.push(msg);
+		if (buffer.length > 15) buffer.shift();
+		client.broadcast(msg);
+	});
+
+	client.on('disconnect', function(){
+		broadcast_clients.pop(client);
+	
+	});
+});
